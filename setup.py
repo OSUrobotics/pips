0e63f5051aacc7caae6dadf248c418fd10fdb738
Aarushi Mehta <mehta.aaru20@gmail.com>
2023-10-11 12:16:51 -0700

    changes by alex to permit importing of pips as module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pips',
+    version='1.0',
+    packages=find_packages(),
+    url='',
+    author='',
+    author_email='',
+    description='PIPs',
+    install_requires=[
+    ],
+)
